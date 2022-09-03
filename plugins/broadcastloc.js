a2c39daf4cbd28c17f358d5c046f39aa1b0652bc
ElainaChann <112780335+ElainaChann@users.noreply.github.com>
2022-09-04 04:32:23 +0700

    Update broadcastloc.js

--- plugins/broadcastloc.js
+++ plugins/broadcastloc.js
@@ -2,7 +2,7 @@ let handler  = async (m, { conn, text }) => {
   let chats = Object.keys(await conn.chats)
   conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_`, m)
   for (let id of chats) {
-       let bcbg = 'https://telegra.ph/file/34c95be3feef20cb61dd5.jpg'
+       let bcbg = 'https://telegra.ph/file/769d9334815eb6dd4b7d5.jpg'
        await conn.delay(1500)
        await conn.send2ButtonLoc(id, bcbg,'*——————「 Broadcast 」——————*\n' + text.trim(), wm, 'Menu', '.menu', 'Owner', '.owner', m)
      }
