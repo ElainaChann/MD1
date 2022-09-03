d26bc2a0220d4e9b56d09f34c83ad1851c58fe9c
ElainaChann <112780335+ElainaChann@users.noreply.github.com>
2022-09-04 04:27:58 +0700

    Update sourcode.js

--- plugins/sourcode.js
+++ plugins/sourcode.js
@@ -2,23 +2,23 @@
 // Jika ada yang Jual sc langsung Di hapus!!
 
 let fetch = require('node-fetch')
-let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, 'https://telegra.ph/file/239f046a928370340401f.jpg',
+let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, 'https://telegra.ph/file/769d9334815eb6dd4b7d5.jpg',
 `Hi Kak @${m.sender.split('@')[0]} 
 
 ◪ 📮 *SCRIPT BOT*
 │ *Script :* 
-│ ╰ https://youtu.be/FHntJPk2PkU
-│ *Mampir Youtube Owner Banh:* 
-│ ╰ https://tinyurl.com/2qsejxnw
+│ ╰ XNXX.com
+│ *Mampir Youtube Owner Banh:*
+│ ╰ https://tinyurl.com/2qsejxn
 ╰──────────═┅═──────────
-`,`📍 *N o t e :* 
-• Jangan lupa mampir Youtube Owner Kak!
-• Jangan Lupa kasih star & kasih credit
+`,`🥴 *N o t e :* 
+• Kepoan Lu
 
 Official By @${'0'.split('@')[0]}
 Powered By @${`${global.owner[0]}`.split('@')[0]}`, 'Pemilik Bot', '#owner')
 handler.help = ['sourcecode']
 handler.tags = ['info']
 handler.command = /^(sourcecode|sc|scbot|script|github)$/i
+handler.group = true
 
 module.exports = handler
