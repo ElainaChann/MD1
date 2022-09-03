d545a9d88ceb6b84c3ff878e1c6730fb7676b980
ElainaChann <112780335+ElainaChann@users.noreply.github.com>
2022-09-04 04:44:05 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -3,17 +3,17 @@ let handler = async (m, {conn}) => {
 const vcard = `BEGIN:VCARD
 VERSION:3.0
 N:;;;
-FN: ᴹᴿ᭄ OscarOffcོ_1ঔৣ꧂
-item.ORG: ᴹᴿ᭄ OscarOffcོ_1ঔৣ꧂
-item1.TEL;waid=6285850539404:6285850539404@s.whatsapp.net
-item1.X-ABLabel:owner selalu ada 
-item2.YOUTUBE;type=INTERNET: https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ
-item2.X-ABLabel:YOUTUBE
-item3.ADR:;;🎍AMERIKA SERIKAT;;;;
+FN: ElainaaOwner
+item.ORG: ElainaaOwner
+item1.TEL;waid=6283830815715:6283830815715@s.whatsapp.net
+item1.X-ABLabel:owner selalu ada kalau penting 
+item2.YOUTUBE;type=INTERNET: https://chat.whatsapp.com/IL34QSFDRT7LyAWiicXpBp
+item2.X-ABLabel:GROUP
+item3.ADR:;;🎍JEPANG;;;;
 item3.X-ABADR:ac
 item3.X-ABLabel:asal kota
-item4.URL:https//github.com/Alpiii22 
-item4.X-ABLabel:Website
+item4.URL:https://i.ibb.co/QkM1p8x/qris.png 
+item4.X-ABLabel:QRISS
 END:VCARD`
 const sentMsg  = await conn.sendMessage(
     m.chat,
@@ -24,7 +24,7 @@ const sentMsg  = await conn.sendMessage(
         }
     }
 )
-let jarot = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg'
+let jarot = 'https://telegra.ph/file/769d9334815eb6dd4b7d5.jpg'
 await conn.send3ButtonImg(m.chat, jarot, `*Hai kak @${m.sender.split('@')[0]} 👋,  Itu nomor owner gw…*`, '📮: jngn di spam kack', '𝙎𝙚𝙬𝙖', '.sewa', '𝘽𝙖𝙘𝙠', '.menu', '𝘿𝙤𝙣𝙖𝙨𝙞', '.donasi', sentMsg)}
 handler.help = ['owner2', 'creator2']
 handler.tags = ['info']
