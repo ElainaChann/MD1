ffe3a272ee82a3307a498629655cf4ab09928559
ElainaChann <112780335+ElainaChann@users.noreply.github.com>
2022-09-04 04:45:02 +0700

    Update tag-owner.js

--- plugins/tag-owner.js
+++ plugins/tag-owner.js
@@ -6,13 +6,13 @@ let handler = async (m, { conn, text }) => {
 
 let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 
-let jarot = 'https://telegra.ph/file/966adedcd3b40f50fa794.jpg'
+let jarot = 'https://telegra.ph/file/769d9334815eb6dd4b7d5.jpg'
 
 await conn.send2ButtonImg(m.chat, jarot, "*BIMLEK🐦*", 'ngapain banh tag” dia', 'OWNER', '.owner', 'KEMBALI', '.menu', m)                        
 
 }
 
-handler.customPrefix = '/@6285850539404/i
+handler.customPrefix = '/@6283830815715/i
 
 handler.command = new RegExp
 
